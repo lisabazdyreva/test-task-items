73867af9eaf5c2122abad52a59fe58fa544f4a9c
lisabazdyreva <lisabazdryreva@gmail.com>
2024-11-21 10:21:56 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  </header>
+  <RouterView />
+  <footer></footer>
+</template>
+
+<style scoped>
+</style>
